6bafe42a48103da926d0b684d67fae62cf607cb0
vitort-ux <vitor.tadeu@souunit.com.br>
2026-03-04 11:24:27 -0500

    tarefa 6

---
+++ server.py
@@ -0,0 +1,46 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+# Import Flask, render_template, request from the flask framework package
+from flask import Flask, render_template, request
+
+# Import the sentiment_analyzer function from the package created
+from EmotionDetection import emotion_detector
+
+# Initiate the flask app
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    ''' This code receives the text from the HTML interface and 
+        runs sentiment analysis over it using sentiment_analysis()
+        function. The output returned shows the label and its confidence 
+        score for the provided text.
+    '''
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Extract the label and score from the response
+    anger_score = response['anger']
+    disgust_score = response['disgust']
+    fear_score = response['fear']
+    joy_score = response['joy']
+    sadness_score = response['sadness']
+    res_dominant_emotion = response['dominant_emotion']
+
+    return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {res_dominant_emotion}."
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    #This functions executes the flask app and deploys it on localhost:5000
+    app.run(host="0.0.0.0", port=5000)
