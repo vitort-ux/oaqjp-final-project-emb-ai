69c6393774efd193f26be902cf321e91adeb8b71
vitort-ux <vitor.tadeu@souunit.com.br>
2026-03-04 07:46:40 -0500

    tarefa 2

---
+++ emotion_detection.py
@@ -0,0 +1,22 @@
+import requests 
+import json
+
+def emotion_detector(text_to_analyse): 
+    # URL of the sentiment analysis service 
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Constructing the request payload in the expected format 
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Custom header specifying the model ID for the sentiment analysis service 
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Sending a POST request to the sentiment analysis API 
+    response = requests.post(url, json=myobj, headers=header)
+    
+    return response.text
+
+
+if __name__ == '__main__':
+    resposta = emotion_detector("Gosto de maça")
+    print(resposta)
